97ac50b1849a4accb352e44a8c45ee8ffe9a6df2
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-01-08 00:11:19 +0800

    feat(docs): add analytics

--- docs/constants/index.ts
+++ docs/constants/index.ts
@@ -4,4 +4,6 @@ export const WEBSITE_TITLE = 'React Forage'
 export const WEBSITE_DESCRIPTION = 'LocalForage Hooks and Providers for React.'
 export const WEBSITE_LOGO_STARTSWITH = 'react-forage'
 
+export const BAIDU_ANALYTICS_ID = '1e82a4de40216c69469dd84e77f77231'
+
 export const REPO_URL = 'https://github.com/chengpeiquan/react-forage'
--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,8 +1,24 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { createBaiduAnalytics } from '@web-analytics/core'
+import { BAIDU_ANALYTICS_ID } from '../constants'
 import '../styles/globals.css'
 import '../styles/custom.css'
 import type { AppProps } from 'next/app'
 
+const baiduAnalytics = createBaiduAnalytics({
+  websiteId: BAIDU_ANALYTICS_ID,
+})
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const { asPath } = useRouter()
+
+  useEffect(() => {
+    baiduAnalytics.trackPageview({
+      pageUrl: asPath,
+    })
+  }, [asPath])
+
   return <Component {...pageProps} />
 }
 
